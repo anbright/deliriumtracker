d3d12dde0b16ef423782a6ea87433157569189df
anbright <anbright@sas.upenn.edu>
2016-10-30 20:12:50 -0400

    added User model

---
+++ app/controllers/provider_controller.rb
@@ -0,0 +1,4 @@
+class ProviderController < ApplicationController
+  def new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
+  get 'provider/new'
+
+	root 'users#new'
+	get 'sign-up', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
