18f42da61fa1e9bc5367200d7a85ab3bbe174a67
anbright <anbright@sas.upenn.edu>
2016-10-30 21:40:16 -0400

    Added header dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
 	root 'sessions#new'
 	get '/sign-up', to: 'users#new'
-    post '/sign-up', to: 'users#create'
-    get '/login', to: 'sessions#new'
-    post '/login', to: 'sessions#create'
-    delete '/logout', to: 'sessions#destroy'
-    
-    get '/help', to: 'users#help'
+  post '/sign-up', to: 'users#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  
+  get '/help', to: 'users#help'
 
   resources :users
 
