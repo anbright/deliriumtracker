a7dc3c75fe171f9d066bec729beafe983a412bf8
anbright <anbright@sas.upenn.edu>
2016-10-30 20:49:39 -0400

    Added login page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,19 @@ class SessionsController < ApplicationController
   end
 
   def create
-  	provider = Provider.find_by(email: params[:session][:email].downcase)
+  	user = User.find_by(email: params[:session][:email].downcase)
+  	if user && user.authenticate(params[:session][:password])
+  		log_in user #generates session for the user id
+  		redirect_to user
+  	else
+  		flash.now[:danger] = "Invalid email or password"
+  		render 'new'
+  	end
   	
   end
 
   def destroy
-
+  	log_out
+  	redirect_to root_url
   end
 end
