62ed03f929bee539e9d2ac4f19cd2196b6f8426e
Deep Kapur <deeptanshuk@gmail.com>
2016-10-31 02:13:20 +0000

    logout change

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
 	root 'sessions#new'
 	get '/sign-up', to: 'users#new'
-  post '/sign-up', to: 'users#create'
-  get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
-  
-  get '/help', to: 'users#help'
-
+	post '/sign-up', to: 'users#create'
+	get '/login', to: 'sessions#new'
+	post '/login', to: 'sessions#create'
+	delete '/logout', to: 'sessions#destroy'
+	
+	get '/help', to: 'users#help'
+	
   resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
