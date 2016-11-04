8857365e941afdd5082e9854591490fe0f149841
anbright <anbright@sas.upenn.edu>
2016-11-04 11:33:02 -0400

    created Patient model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161031001039) do
+ActiveRecord::Schema.define(version: 20161104151927) do
+
+  create_table "patients", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "providers", force: :cascade do |t|
     t.string   "name"
