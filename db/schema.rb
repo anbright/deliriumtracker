2d545b58d2952865530132a6209e5c9511045154
Andrew Bright <anbright@sas.upenn.edu>
2016-11-17 23:13:23 +0000

    Changed reference from users to patients for accelerometers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161113020145) do
+ActiveRecord::Schema.define(version: 20161117230736) do
 
   create_table "accelerometers", force: :cascade do |t|
     t.integer  "time",       limit: 8
@@ -19,8 +19,8 @@ ActiveRecord::Schema.define(version: 20161113020145) do
     t.float    "z"
     t.datetime "created_at",           null: false
     t.datetime "updated_at",           null: false
-    t.integer  "user_id"
-    t.index ["user_id"], name: "index_accelerometers_on_user_id"
+    t.integer  "patient_id"
+    t.index ["patient_id"], name: "index_accelerometers_on_patient_id"
   end
 
   create_table "api_keys", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,12 +11,12 @@
 # 	])
 
 User.create!(name: 'test', email: 'test@example.com', password: 'password')
-User.create!(name: 'foo', email: 'foo@bar.com', password: 'password1')
+User.create!(name: 'foo', email: 'foo@bar.com', password: 'password')
 User.first.patients.create!(name: 'Joe Smith')
 
 
 ##Creating Accelerometer Data
-Accelerometer.create!(time: 0, user_id: 1, x: 2.6340000629425, y:-0.143999993801116, z:9.61499977111816)
+Accelerometer.create!(time: 0, patient_id: 1, x: 2.6340000629425, y:-0.143999993801116, z:9.61499977111816)
 
 time = 1470938891811 
 
@@ -26,6 +26,6 @@ end
 
 5_000.times do
 	time += 5000
-	Accelerometer.create!(time: time, user_id: 1, x: get_rand, y:get_rand, z: get_rand)
+	Accelerometer.create!(time: time, patient_id: 1, x: get_rand, y:get_rand, z: get_rand)
 end
 
