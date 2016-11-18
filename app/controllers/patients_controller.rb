776812bb7bd9730ed04738a144d9ec2f8e45d989
dkkapur <deeptanshuk@gmail.com>
2016-11-18 06:36:59 +0000

    akshay is genius

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -4,20 +4,24 @@ class PatientsController < ApplicationController
       	@patient = Patient.find(params[:id])
       	@data = Accelerometer.all
       	
+      	@patient_data = Accelerometer.where("patient_id = ?", params[:id])
+      	
       	mag = magnitude
         @avg_points = Array.new
         @time = Array.new
-        timestamp = @data.pluck(:time)
+        timestamp = @patient_data.pluck(:time)
         while mag.count>0
             @avg_points << average_min(mag.shift(12))
             @time << timestamp.shift(12).first
         end
+        @first = @avg_points.first(5)
+        @hash = @time.zip(@avg_points).to_h
     end
     
         
     def magnitude
         mag = Array.new
-        @data.each do |d|
+        @patient_data.each do |d|
             mag << d.x.abs + d.y.abs + d.z.abs
         end
         return mag
--- db/seeds.rb
+++ db/seeds.rb
@@ -20,7 +20,7 @@ Patient.create!(name: 'Joe Smith')
 ##Creating Accelerometer Data
 Accelerometer.create!(time: 0, patient_id: 1, x: 2.6340000629425, y:-0.143999993801116, z:9.61499977111816)
 
-time = 1470938891811 
+time = Time.now.utc 
 
 def get_rand
 	rand(-10.0..10.0)
@@ -28,6 +28,6 @@ end
 
 5_000.times do
 	time += 5000
-	Accelerometer.create!(time: time, patient_id: 1, x: get_rand, y:get_rand, z: get_rand)
+	Accelerometer.create!(time: Time.at(time), patient_id: 1, x:get_rand, y:get_rand, z:get_rand)
 end
 
